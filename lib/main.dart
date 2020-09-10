9a6a6c75cf9c9edbf19e419c956c34d96bca6caf
Beomzu.Kim <49941253+beomzuKim@users.noreply.github.com>
2020-09-11 00:29:31 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:instagram_clon/root_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
+void main()  =>  runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
